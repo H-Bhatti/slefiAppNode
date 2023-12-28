db60e92d323ac502d2aeede0acbd0f60eff94171
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2023-12-28 21:28:40 +0400

    added database functionality with nedb

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
 const express = require ("express")
-const fs = require ("fs")
+const fs = require ("fs");
+const { parse } = require("path");
 const app = express();
+const Datastore = require('nedb')
 // whole express library comes as a functoin
 
 app.listen(3000, ()=>console.log("listening"))
 app.use(express.static("public"))
 
+
+const database = new Datastore("database.db");
+database.loadDatabase();
+
+
 // post
 app.use(express.json({limit: "1mb"}))
 app.post("/api", (request, response) => {
     console.log("I got a request!");
-    console.log(request.body);
+    // console.log(request.body);
     const data = request.body;
-    response.json({
-      status: "success",
-      latitude: data.lat,
-      longitude: data.lon,
-    });
-    fs.appendFile('./logs.txt', `${data.lat}, ${data.lon}
-    `, (err) => {
+    const time = Date.now();
+    data.timeStamp = time;
+    fs.appendFile('./logs.txt', JSON.stringify(data), (err) => {
         if (err) throw err;
         console.log('Line added to the file');
       });
+    database.insert(data);
+    response.json({
+      status: "success",
+      time: data.timeStamp,
+      latitude: data.lat,
+      longitude: data.lon,
+    });
+
   });
 
 //  const text =  fs.readFileSync("./logs.txt","utf-8");

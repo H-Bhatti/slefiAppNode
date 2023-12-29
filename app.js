a12eb5f8e36597bd686d8dd5a4c382a487648ab2
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2023-12-29 19:55:47 +0400

    adding comments

--- app.js
+++ app.js
@@ -7,24 +7,25 @@ const Datastore = require('nedb')
 
 app.listen(3000, ()=>console.log("listening"))
 app.use(express.static("public"))
+// starting express local server
 
 
 const database = new Datastore("database.db");
 database.loadDatabase();
+// initilizing the database and calling it
 
 
 // post
-app.use(express.json({limit: "1mb"}))
-app.post("/api", (request, response) => {
+app.use(express.json({limit: "1mb"}))   
+// express feature to limit the inocoming document size to 1 mb
+app.post("/api", (request, response) => {           //getting the post request with fetch in our api
     console.log("I got a request!");
     // console.log(request.body);
     const data = request.body;
-    const time = Date.now();
+    const time = Date.now();        //getting a timestamp
     data.timeStamp = time;
-    fs.appendFile('./logs.txt', JSON.stringify(data), (err) => {
-        if (err) throw err;
-        console.log('Line added to the file');
-      });
+    // pushing it into thedatabase
+    
     database.insert(data);
     response.json({
       status: "success",
